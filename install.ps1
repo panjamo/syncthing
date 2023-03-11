892127deb4374fa8bbc6994fdcd14baff815e47e
Jochen Schieberlein <jochen.schieberlein@thinprint.com>
2023-03-11 22:23:25 +0100

    nun fertig

--- install.ps1
+++ install.ps1
@@ -1,22 +1,43 @@
-Start-Process -FilePath '.\syncthing.exe' -ArgumentList 'cli operations shutdown' -Wait
+# @REM https://docs.syncthing.net/rest/config.html
+# @REM https://www.mankier.com/7/syncthing-rest-api
+
+[XML]$config = Get-Content ('.\config\config.xml')
+
+Invoke-WebRequest `
+    -Uri "http://127.0.0.1:8384/rest/system/shutdown" `
+    -Method "POST" `
+    -Headers @{ "X-API-Key" = $config.configuration.gui.apikey }
+
 Start-Sleep -Seconds 3
-# Remove-Item -LiteralPath ($env:LOCALAPPDATA + '\Syncthing') -Force -Recurse
+
+Remove-Item  ([Environment]::GetFolderPath('Desktop')+'\Syncthing.lnk')
+Remove-Item '.\config' -Force -Recurse -ErrorAction SilentlyContinue
+
 New-Item -Path '.\config' -ItemType Directory -ErrorAction SilentlyContinue
 
 Start-Process -FilePath '.\syncthing.exe'  -WindowStyle Minimized -ArgumentList 'serve --no-default-folder --home=.\config'
 Start-Sleep -Seconds 3
-# Start-Process -FilePath '.\syncthing.exe' -ArgumentList 'cli operations shutdown' -Wait
-[XML]$config = Get-Content ('.\config\config.xml')
-#$config.configuration.gui.apikey = "iVPAH5Vi7VTmdoJKiyxsRZc4MTfzk7qy"
-#$config.save($env:LOCALAPPDATA + '\Syncthing\config.xml')
-# & .\syncthing.exe cli operations shutdown
-#Start-Process -FilePath '.\syncthing.exe'  -WindowStyle Minimized -ArgumentList 'serve --no-default-folder'
-#Start-Sleep -Seconds 5
 
-& .\syncthing.exe cli config defaults device auto-accept-folders set true --home=.\config
-& .\syncthing.exe cli config defaults folder rescan-intervals set 600 --home=.\config
-& .\syncthing.exe cli config defaults device compression set always --home=.\config
-& .\syncthing.exe cli config defaults folder path set c:\tmp --home=.\config
+[XML]$config = Get-Content ('.\config\config.xml')
+$defaultFolder = (Invoke-WebRequest -Uri "http://127.0.0.1:8384/rest/config/defaults/folder"  -Method "GET"  -Headers @{ "X-API-Key" = $config.configuration.gui.apikey }) | ConvertFrom-Json
+$defaultFolder.rescanIntervalS = 600
+$defaultFolder.path = 'c:\tmp'
+$defaultFolder.order = 'smallestFirst'
+Invoke-WebRequest `
+    -Uri "http://127.0.0.1:8384/rest/config/defaults/folder" `
+    -Method "PUT" `
+    -Headers @{ "X-API-Key" = $config.configuration.gui.apikey } `
+    -body ($defaultFolder | ConvertTo-Json)
+
+
+$defaultDevice = (Invoke-WebRequest -Uri "http://127.0.0.1:8384/rest/config/defaults/device"  -Method "GET"  -Headers @{ "X-API-Key" = $config.configuration.gui.apikey }) | ConvertFrom-Json
+$defaultDevice.autoAcceptFolders = $true
+$defaultDevice.compression = 'always'
+Invoke-WebRequest `
+    -Uri "http://127.0.0.1:8384/rest/config/defaults/device" `
+    -Method "PUT" `
+    -Headers @{ "X-API-Key" = $config.configuration.gui.apikey } `
+    -body ($defaultDevice | ConvertTo-Json) `
 
 $device = @'
 {
@@ -53,15 +74,7 @@ $s.TargetPath=((Get-Location).ToString()+'\syncthing.exe');
 $s.Arguments='--home='+(Get-Location).ToString()+'\config'
 $s.Save()
 
-
-# syncthing.exe cli config devices add^
-#   --device-id W4VTQIB-HAI2DHP-6ZCI5XD-EOV6NNI-3PGCRJJ-4MNHZ53-QKNXFF2-GA23HAN^
-#   --name LAPTOP-JOSCH^
-#   --introducer^
-#   --auto-accept-folders^
-#   --compression always
-  
-# mkdir c:\tmp
+  # mkdir c:\tmp
 # del /A:H /q /f c:\tmp\.stignore
 # echo // !/DebugTools/syncthing-excutables/install.cmd>>c:\tmp\.stignore
 # echo // /DebugTools/syncthing-excutables>>c:\tmp\.stignore
@@ -71,68 +84,3 @@ $s.Save()
 # echo /*/**>>c:\tmp\.stignore
 # echo /BIN.zip>>c:\tmp\.stignore
 # echo /*.dmp>>c:\tmp\.stignore
-# syncthing.exe cli config folders add --id TMP-%COMPUTERNAME% --path c:\tmp --label TMP-%COMPUTERNAME% --paused
-
-# @REM https://docs.syncthing.net/rest/config.html
-# @REM https://www.mankier.com/7/syncthing-rest-api
-
-# @REM @curl -H "X-API-Key: iVPAH5Vi7VTmdoJKiyxsRZc4MTfzk7qy" -X POST http://127.0.0.1:8384/rest/db/scan
-# @REM @curl -H "X-API-Key: iVPAH5Vi7VTmdoJKiyxsRZc4MTfzk7qy" -X GET http://127.0.0.1:8384/rest/config/devices
-# @REM @curl -H "X-API-Key: 3JCpWS9zsve5HFyMxryvGRETaRZATbTo" -X PATCH http://127.0.0.1:8384/rest/config/devices/W4VTQIB-HAI2DHP-6ZCI5XD-EOV6NNI-3PGCRJJ-4MNHZ53-QKNXFF2-GA23HAN -H 'Content-Type: application/json' -d "{""addresses"": [""dynamic""]}"
-# @REM @curl -H "X-API-Key: 3JCpWS9zsve5HFyMxryvGRETaRZATbTo" -X PATCH http://127.0.0.1:8384/rest/config/devices/W4VTQIB-HAI2DHP-6ZCI5XD-EOV6NNI-3PGCRJJ-4MNHZ53-QKNXFF2-GA23HAN -H 'Content-Type: application/json' -d "{""paused"": ""true""}"
-
-
-
-
-
-
-
-
-
-# curl -H "X-API-Key: iVPAH5Vi7VTmdoJKiyxsRZc4MTfzk7qy" `
-# -X PATCH http://127.0.0.1:8384/rest/config/devices/64HAS4W-H3OCIIC-6RHPVTF-XFFBCHP-H3BOOTZ-PTYAW5L-RKD2E4Y-WNNGOAY `
-# -H 'Content-Type: application/json' `
-# -d '{"paused": true}'
-
-# curl -H "X-API-Key: iVPAH5Vi7VTmdoJKiyxsRZc4MTfzk7qy" `
-# -X PATCH http://127.0.0.1:8384/rest/config/devices/64HAS4W-H3OCIIC-6RHPVTF-XFFBCHP-H3BOOTZ-PTYAW5L-RKD2E4Y-WNNGOAY `
-# -H 'Content-Type: application/json' `
-# -d @'
-# {
-#     "addresses": [
-#       "tcp://panjamo.selfhost.eu:22000"
-#     ]
-#   }
-# '@
-
-# & curl -H "X-API-Key: iVPAH5Vi7VTmdoJKiyxsRZc4MTfzk7qy" `
-# -X PUT http://127.0.0.1:8384/rest/config/devices/64HAS4W-H3OCIIC-6RHPVTF-XFFBCHP-H3BOOTZ-PTYAW5L-RKD2E4Y-WNNGOAY `
-# -H 'Content-Type: application/json' `
-# -d @'
-# {
-#     "deviceID": "64HAS4W-H3OCIIC-6RHPVTF-XFFBCHP-H3BOOTZ-PTYAW5L-RKD2E4Y-WNNGOAY",
-#     "name": "Pixel 5",
-#     "addresses": [
-#       "tcp://127.0.0.1:4545"
-#     ],
-#     "compression": "always",
-#     "certName": "",
-#     "introducer": false,
-#     "skipIntroductionRemovals": false,
-#     "introducedBy": "",
-#     "paused": false,
-#     "allowedNetworks": [],
-#     "autoAcceptFolders": false,
-#     "maxSendKbps": 0,
-#     "maxRecvKbps": 0,
-#     "ignoredFolders": [],
-#     "maxRequestKiB": 0,
-#     "untrusted": false,
-#     "remoteGUIPort": 0
-#   }
-# '@
-
-# # curl -H "X-API-Key: iVPAH5Vi7VTmdoJKiyxsRZc4MTfzk7qy" `
-# #     -X GET http://127.0.0.1:8384/rest/config/devices/64HAS4W-H3OCIIC-6RHPVTF-XFFBCHP-H3BOOTZ-PTYAW5L-RKD2E4Y-WNNGOAY
- 
-# # curl -H "X-API-Key: iVPAH5Vi7VTmdoJKiyxsRZc4MTfzk7qy" -X GET http://127.0.0.1:8384/rest/config/devices    
